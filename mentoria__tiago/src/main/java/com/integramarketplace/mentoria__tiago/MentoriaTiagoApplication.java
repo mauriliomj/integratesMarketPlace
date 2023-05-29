fb25fa523d71f5a076c746802095e59078c03ee9
maurilio <mauriliojose249@gmail.com>
2023-05-29 10:35:00 -0300

    Reiniciando projeto para sanar erros na jdk

---
+++ mentoria__tiago/src/main/java/com/integramarketplace/mentoria__tiago/MentoriaTiagoApplication.java
@@ -0,0 +1,13 @@
+package com.integramarketplace.mentoria__tiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoriaTiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoriaTiagoApplication.class, args);
+	}
+
+}
---
+++ mentoria__tiago/src/test/java/com/integramarketplace/mentoria__tiago/MentoriaTiagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integramarketplace.mentoria__tiago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoriaTiagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
